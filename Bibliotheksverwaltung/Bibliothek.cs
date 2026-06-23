d7da281cf8ede1afc58356750b56a131b08b9f8d
Marvin <marvinstiegler@gmx.at>
2026-06-23 12:03:46 +0200

    Bücher anzeigen hinzugefügt

---
+++ Bibliotheksverwaltung/Bibliothek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bibliotheksverwaltung
+{
+    internal class Bibliothek
+    {
+        List<Buch> buecherListe = new List<Buch>();
+
+
+        public void BuchHinzufuegen(string titel, string autor, bool verfügbarkeit)
+        {
+            Buch buch = new Buch(titel, autor, verfügbarkeit);
+            buecherListe.Add(buch);
+            
+        }
+
+        public void BuecherAnzeigen()
+        {
+            foreach (Buch buch in buecherListe)
+            {
+                Console.WriteLine("Titel: {0}, Autor: {1}, Verfügbarkeit: {2}", buch.Titel, buch.Autor, buch.Verfügbarkeit);
+            }
+        }
+    }
+}
--- Bibliotheksverwaltung/Program.cs
+++ Bibliotheksverwaltung/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bibliothek bibliothek = new Bibliothek();
+            bibliothek.BuchHinzufuegen("Der Herr der Ringe", "J.R.R. Tolkin", true);
+            bibliothek.BuchHinzufuegen("Harry Potter", "J.K. Rowling", false);
+            bibliothek.BuchHinzufuegen("Die unendliche Geschichte", "Michael Ende", true);
+            bibliothek.BuchHinzufuegen("Der kleine Prinz", "Antoine de Saint-Exupéry", true);
+            bibliothek.BuchHinzufuegen("1984", "George Orwell", false);
+            bibliothek.BuchHinzufuegen("Die Verwandlung", "Franz Kafka", true);
+            bibliothek.BuchHinzufuegen("Faust", "Johann Wolfgang von Goethe", true);
+            bibliothek.BuecherAnzeigen();   
         }
     }
 }
